5f8e7115e2136059b52b491cbebfa8d229ec080d
Peter Stuifzand <peter@stuifzand.eu>
2019-08-08 23:44:09 +0200

    Allow command line argument
    Signed-off-by: Peter Stuifzand <peter@stuifzand.eu>

--- generate.php
+++ generate.php
@@ -4,7 +4,10 @@ require_once 'vendor/autoload.php';
 use Stuifzand\TestGenerator\Model\Parser;
 
 $parser = new Parser();
-$info   = $parser->parse(file_get_contents(__DIR__ . '/../magento2/kata3/vendor/magento/module-catalog/Model/CategoryRepository.php'));
+
+$filename = $argv[1];
+
+$info = $parser->parse(file_get_contents($filename));
 
 $generator = new \Stuifzand\TestGenerator\Model\Generator();
 echo $generator->generate($info);
