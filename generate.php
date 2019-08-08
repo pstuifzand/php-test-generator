f12dbce3676ca427f071ee978c27d272312d0a3f
Peter Stuifzand <peter@stuifzand.eu>
2019-08-08 23:44:26 +0200

    Reorder variables
    Signed-off-by: Peter Stuifzand <peter@stuifzand.eu>

--- generate.php
+++ generate.php
@@ -3,11 +3,10 @@ require_once 'vendor/autoload.php';
 
 use Stuifzand\TestGenerator\Model\Parser;
 
-$parser = new Parser();
-
 $filename = $argv[1];
 
-$info = $parser->parse(file_get_contents($filename));
+$parser = new Parser();
+$info   = $parser->parse(file_get_contents($filename));
 
 $generator = new \Stuifzand\TestGenerator\Model\Generator();
 echo $generator->generate($info);
